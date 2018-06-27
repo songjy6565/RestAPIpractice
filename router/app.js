46b7839f2099806c4f5e2ae905a2120f54adc0ce
songjy6565 <songjy6565@kaist.ac.kr>
2018-06-27 20:44:18 +0900

    w/o xss

--- router/app.js
+++ router/app.js
@@ -14,85 +14,85 @@ module.exports = function(app, fs, db)
 	
 	app.get('/api/v1/articles',function(req, res, next) {
 		db.query('SELECT * FROM article', function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : result});
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : result});
 		});
 	});
 
 	app.post('/api/v1/articles',function(req, res, next) {
 		var arti = {'author' : req.body.author, 'content' : req.body.content, 'date' : req.body.date};
 		db.query('INSERT INTO article SET ?', arti ,function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
 			else{
-				db.query('SELECT id FROM article WHERE content='+mysql.escape(req.body.content),function(err1,result1){
-					if (err) res.json({"status": 500, "error": err1, "response" : null});
-					else res.json({"status": 200, "error": null, "response" : result1});
-				}); //need to set primary key(author,content,date) and compare all of them.
+				db.query('SELECT LAST_INSERT_ID()',function(err1,result1){
+					if (err) res.status(500).json({"status": 500, "error": err1, "response" : null});
+					else res.status(200).json({"status": 200, "error": null, "response" : result1});
+				});
 			}
 		});
 	});
 	
 	app.get('/api/v1/articles/:number', function(req, res, next) {
 		db.query('SELECT * FROM article WHERE id='+mysql.escape(req.params.number),function(err,result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : result});
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : result});
 		});
 	});
 	
 	app.put('/api/v1/articles/:number', function(req, res, next) {
 		var arti = {'author' : req.body.author, 'content' : req.body.content, 'date' : req.body.date};
 		db.query('UPDATE article SET ? WHERE id='+mysql.escape(req.params.number), arti, function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : req.params.number});
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : req.params.number});
 		});
 	});
 
 	app.delete('/api/v1/articles/:number', function(req, res, next) {
 		db.query('DELETE FROM article WHERE id='+mysql.escape(req.params.number), function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : req.params.number});
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : req.params.number});
 		});
 	});
 
 	app.get('/api/v1/articles/:number/comments', function(req, res, next) {
 		db.query('SELECT * FROM comment WHERE parentid='+mysql.escape(req.params.number),function(err,result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : result});
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : result});
 		});
 	});
 
 	app.post('/api/v1/articles/:number/comments', function(req, res, next) {
 		var comm = {'author' : req.body.author, 'content' : req.body.content, 'date' : req.body.date, 'parentid' : req.params.number};
 		db.query('INSERT INTO comment SET ?', comm, function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
 			else{
-				db.query('SELECT id FROM comment WHERE content='+mysql.escape(req.body.content),function(err1,result1){
-					if (err) res.json({"status": 500, "error": err1, "response" : null});
-					else res.json({"status": 200, "error": null, "response" : result1});
-				}); //need to set primary key(author,content,date,parentid) and compare all of them.
+				db.query('SELECT LAST_INSERT_ID()',function(err1,result1){
+					if (err) res.status(500).json({"status": 500, "error": err1, "response" : null});
+					else res.status(200).json({"status": 200, "error": null, "response" : result1});
+				});
 			}
 		});
 	});
 
 	app.get('/api/v1/articles/:number/comments/:child', function(req, res, next) {
-		db.query('SELECT * FROM comment WHERE parentid=? ORDER BY id LIMIT ?,1',[req.params.number,(req.params.child - 1)],function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : result});
+		db.query('SELECT * FROM comment WHERE id='+mysql.escape(req.params.child),function(err, result){
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : result});
 		});
 	});
 
 	app.put('/api/v1/articles/:number/comments/:child', function(req, res, next) {
 		var comm = {'author' : req.body.author, 'content' : req.body.content, 'date' : req.body.date};
-		db.query('UPDATE comment INNER JOIN (SELECT id as target FROM comment WHERE parentid = ? ORDER BY id LIMIT ?,1) a ON a.target = id SET ?',[req.params.number,(req.params.child-1),comm], function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : req.params.child});
+		db.query('UPDATE comment SET ? WHERE id='+mysql.escape(req.params.child),comm, function(err, result){
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : req.params.child});
 		});
 	});
 
 	app.delete('/api/v1/articles/:number/comments/:child', function(req, res, next) {
-		db.query('DELETE FROM comment WHERE id = (SELECT id FROM (SELECT id FROM comment WHERE parentid = ? ORDER BY id LIMIT ?,1) as a',[req.params.number,(req.params.child-1)], function(err, result){
-			if (err) res.json({"status": 500, "error": err, "response" : null});
-			else res.json({"status": 200, "error": null, "response" : req.params.child});
+		db.query('DELETE FROM comment WHERE id ='+mysql.escape(req.params.child), function(err, result){
+			if (err) res.status(500).json({"status": 500, "error": err, "response" : null});
+			else res.status(200).json({"status": 200, "error": null, "response" : req.params.child});
 		});
 	});
 };
--- server.js
+++ server.js
@@ -15,4 +15,29 @@ var server = app.listen(3500, function(){
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next){
+	if (req.headers.authorization || req.headers.authorization.search('Basic ') ==0){
+		if(new Buffer(req.headers.authorization.split(' ')[1], 'base64').toString() == 'jupsu:password123'){
+			next();
+			return;
+		}
+	}
+	res.status(401).json({'status':401, 'error':Authorization Failed.});
+});
+
+app.use(function(req, res, next){
+	if (req.method == 'PUT' || req.method == 'POST'){
+		var author = req.body.author;
+		var content = req.body.content;
+		var date = req.body.date;
+		if(!author || typeof author != 'string' || author.length > 255)
+			return res.status(400).json({'status':400,'error':request body is wrong(author)});
+		if(!content || typeof content != 'string' || content.length > 255)
+			return res.status(400).json({'status':400,'error':request body is wrong(content)});
+		if(!date || typeof date != 'string' || date.length > 255)
+			return res.status(400).json({'status':400,'error':request body is wrong(date)});
+	}
+	next();
+});
+
 var router = require('./router/app')(app, fs, db);
