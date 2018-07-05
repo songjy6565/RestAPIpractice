51e30797e921a5fb10b53ac76b95b952d57c0f67
songjy6565 <songjy6565@kaist.ac.kr>
2018-07-05 15:13:58 +0900

    combine js task.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -13,7 +13,7 @@ import cache from 'gulp-file-cache';
 let Cache = new cache();
 let c = false;
 
-gulp.task('default',['clean','js1','js2','js3','js4','html1','watch'], () => {
+gulp.task('default',['clean','js','html','watch'], () => {
 	return gulputil.log('Gulp is using');
 });
 
@@ -22,117 +22,48 @@ const DIR = {
 	DEST : 'dist'
 };
 const SRC = {
-	JS1 : DIR.SRC + '/*.js',
-	JS2 : DIR.SRC + '/router/*.js',
-	JS3 : DIR.SRC + '/lib/*.js',
-	JS4 : DIR.SRC + '/lib/middleware/*.js',
-	HTML1 : DIR.SRC + '/views/*.html'
+	JS : DIR.SRC + '/**/*.js',
+	HTML : DIR.SRC + '/**/*.html'
 };
 
 const DEST = {
-	JS1 : DIR.DEST + '/',
-	JS2 : DIR.DEST + '/router',
-	JS3 : DIR.DEST + '/lib',
-	JS4 : DIR.DEST + '/lib/middleware',
-	HTML1 : DIR.DEST + '/views'
+	JS : DIR.DEST + '/',
+	HTML : DIR.DEST + '/'
 };
 
-gulp.task('js1', () => {
+gulp.task('js', () => {
 	if(c){
-		return gulp.src(SRC.JS1)
+		return gulp.src(SRC.JS)
 		.pipe(Cache.filter())
 		.pipe(babel({
 			presets:['es2015']
 		}))
 		.pipe(uglify())
 		.pipe(Cache.cache())
-		.pipe(gulp.dest(DEST.JS1));
+		.pipe(gulp.dest(DEST.JS));
 	}
 	else{
-		return gulp.src(SRC.JS1)
+		return gulp.src(SRC.JS)
 		.pipe(babel({
 			presets:['es2015']
 		}))
 		.pipe(uglify())
-		.pipe(gulp.dest(DEST.JS1));
+		.pipe(gulp.dest(DEST.JS));
 	}
 });
 
-gulp.task('js2', () => {
+gulp.task('html', () => {
 	if(c){
-                return gulp.src(SRC.JS2)
-                .pipe(Cache.filter())
-                .pipe(babel({
-                        presets:['es2015']
-                }))
-                .pipe(uglify())
-                .pipe(Cache.cache())
-                .pipe(gulp.dest(DEST.JS2));
-        }
-        else{
-                return gulp.src(SRC.JS2)
-                .pipe(babel({
-                        presets:['es2015']
-                }))
-                .pipe(uglify())
-                .pipe(gulp.dest(DEST.JS2));
-        }
-});
-
-gulp.task('js3', () => {
-	if(c){
-                return gulp.src(SRC.JS3)
-                .pipe(Cache.filter())
-                .pipe(babel({
-                        presets:['es2015']
-                }))
-                .pipe(uglify())
-                .pipe(Cache.cache())
-                .pipe(gulp.dest(DEST.JS3));
-        }
-        else{
-                return gulp.src(SRC.JS3)
-                .pipe(babel({
-                        presets:['es2015']
-                }))
-                .pipe(uglify())
-                .pipe(gulp.dest(DEST.JS3));
-        }
-});
-
-gulp.task('js4', () => {
-	if(c){
-                return gulp.src(SRC.JS4)
-                .pipe(Cache.filter())
-                .pipe(babel({
-                        presets:['es2015']
-                }))
-                .pipe(uglify())
-                .pipe(Cache.cache())
-                .pipe(gulp.dest(DEST.JS4));
-        }
-        else{
-                return gulp.src(SRC.JS4)
-                .pipe(babel({
-                        presets:['es2015']
-                }))
-                .pipe(uglify())
-                .pipe(gulp.dest(DEST.JS4));
-        }
-});
-
-gulp.task('html1', () => {
-	if(c){
-                return gulp.src(SRC.HTML1)
+                return gulp.src(SRC.HTML)
                 .pipe(Cache.filter())
 		.pipe(htmlmin({collapseWhitespace:true}))
                 .pipe(Cache.cache())
-                .pipe(gulp.dest(DEST.HTML1));
+                .pipe(gulp.dest(DEST.HTML));
         }
         else{
-        	return gulp.src(SRC.HTML1)
+        	return gulp.src(SRC.HTML)
                 .pipe(htmlmin({collapseWhitespace:true}))
-                .pipe(gulp.dest(DEST.HTML1));
+                .pipe(gulp.dest(DEST.HTML));
 	}
 });
 
@@ -143,9 +74,6 @@ gulp.task('clean', () => {
 
 gulp.task('watch', () => {
 	c = true;
-	gulp.watch(SRC.JS1, ['js1']),
-	gulp.watch(SRC.JS2, ['js2']),
-	gulp.watch(SRC.JS3, ['js3']),
-	gulp.watch(SRC.JS4, ['js4']),
-	gulp.watch(SRC.HTML1, ['html1'])
+	gulp.watch(SRC.JS, ['js']),
+	gulp.watch(SRC.HTML, ['html'])
 });
